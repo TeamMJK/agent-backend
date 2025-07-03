8e526f1f17bc7ee718e589fe1f93c28bb5814a44
y22jun <tlsdpwns0612@naver.com>
2025-07-03 14:06:00 +0900

    Setting: 초기 세팅

---
+++ src/main/java/team/mjk/agent/global/exception/CustomException.java
@@ -0,0 +1,21 @@
+package team.mjk.agent.global.exception;
+
+import lombok.Getter;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ExceptionCode code;
+
+    public CustomException(ExceptionCode code) {
+        super(code.getMessage());
+        this.code = code;
+    }
+
+    public boolean isServerError() {
+        return code.getStatus().equals(INTERNAL_SERVER_ERROR);
+    }
+
+}
---
+++ src/main/java/team/mjk/agent/global/exception/ExceptionCode.java
@@ -0,0 +1,13 @@
+package team.mjk.agent.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ExceptionCode {
+
+    HttpStatus getStatus();
+
+    String getCode();
+
+    String getMessage();
+
+}
