70cf313078d8864045db5cc2c5dc665b55aac177
myeongjaeking <112608799+myeongjaeking@users.noreply.github.com>
2025-07-11 16:37:43 +0900

    Chore: 초대 코드 생성 스웨거 수정

--- src/main/java/team/mjk/agent/domain/company/presentation/CompanyDocsController.java
+++ src/main/java/team/mjk/agent/domain/company/presentation/CompanyDocsController.java
@@ -17,56 +17,55 @@ import team.mjk.agent.domain.company.dto.request.CompanySaveRequest;
 @RequestMapping("/companies")
 public interface CompanyDocsController {
 
-    @Operation(summary = "초대 코드 생성", description = "회사 초대 코드를 생성합니다.")
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "201",
-                    description = "초대 코드 생성 성공",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
-            )
-    })
-    ResponseEntity<String> createInvitationCode(
-            @Parameter(description = "회사 ID", required = true) Long companyId
-    );
+  @Operation(summary = "초대 코드 생성", description = "회사 초대 코드를 생성합니다.")
+  @ApiResponses({
+      @ApiResponse(
+          responseCode = "201",
+          description = "초대 코드 생성 성공",
+          content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
+      )
+  })
+  ResponseEntity<String> createInvitationCode(
+      @Parameter(hidden = true) Long memberID
+  );
 
-    @Operation(summary = "회사 생성", description = "새로운 회사를 생성합니다.")
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "201",
-                    description = "회사 생성 성공",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Long.class))
-            )
-    })
-    ResponseEntity<Long> createCompany(
-            @Parameter(hidden = true) Long memberId,
-            @RequestBody(description = "회사 생성 요청 DTO", required = true) CompanySaveRequest request
-    );
+  @Operation(summary = "회사 생성", description = "새로운 회사를 생성합니다.")
+  @ApiResponses({
+      @ApiResponse(
+          responseCode = "201",
+          description = "회사 생성 성공",
+          content = @Content(mediaType = "application/json", schema = @Schema(implementation = Long.class))
+      )
+  })
+  ResponseEntity<Long> createCompany(
+      @Parameter(hidden = true) Long memberId,
+      @RequestBody(description = "회사 생성 요청 DTO", required = true) CompanySaveRequest request
+  );
 
-    @Operation(summary = "회사 가입", description = "초대 코드를 이용해 회사에 가입합니다.")
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "회사 가입 성공",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
-            )
-    })
-    ResponseEntity<String> joinCompany(
-            @Parameter(hidden = true) Long memberId,
-            @RequestBody(description = "회사 초대 코드 요청 DTO", required = true) CompanyInvitationCodeRequest request
-    );
+  @Operation(summary = "회사 가입", description = "초대 코드를 이용해 회사에 가입합니다.")
+  @ApiResponses({
+      @ApiResponse(
+          responseCode = "200",
+          description = "회사 가입 성공",
+          content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
+      )
+  })
+  ResponseEntity<String> joinCompany(
+      @Parameter(hidden = true) Long memberId,
+      @RequestBody(description = "회사 초대 코드 요청 DTO", required = true) CompanyInvitationCodeRequest request
+  );
 
-    @Operation(summary = "회사 정보", description = "회사 정보에서 회사 이름을 가져옵니다.")
-    @ApiResponses({
-        @ApiResponse(
-            responseCode = "200",
-            description = "회사 정보 가져오기 성공",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
-        )
-    })
-    ResponseEntity<String> getCompany(
-        @Parameter(hidden = true) Long memberId
-    );
-
+  @Operation(summary = "회사 정보", description = "회사 정보에서 회사 이름을 가져옵니다.")
+  @ApiResponses({
+      @ApiResponse(
+          responseCode = "200",
+          description = "회사 정보 가져오기 성공",
+          content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
+      )
+  })
+  ResponseEntity<String> getCompany(
+      @Parameter(hidden = true) Long memberId
+  );
 
 
 }
