cb97521e415cac492ff97395e8444b49a97861f4
y22jun <tlsdpwns0612@naver.com>
2025-08-27 16:56:39 +0900

    refactor: 초대코드 생성 -> 초대코드 생성, 이메일 전송 기능 구현

--- src/main/java/team/mjk/agent/domain/company/application/CompanyService.java
+++ src/main/java/team/mjk/agent/domain/company/application/CompanyService.java
@@ -1,21 +1,26 @@
 package team.mjk.agent.domain.company.application;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.mjk.agent.domain.company.domain.Company;
 import team.mjk.agent.domain.company.domain.CompanyRepository;
 import team.mjk.agent.domain.company.dto.request.CompanyInvitationCodeRequest;
+import team.mjk.agent.domain.company.dto.request.CompanyInvitationEmailRequest;
 import team.mjk.agent.domain.company.dto.request.CompanySaveRequest;
+import team.mjk.agent.domain.company.dto.response.CompanyInvitationEmailResponse;
 import team.mjk.agent.domain.company.dto.response.CompanyJoinResponse;
 import team.mjk.agent.domain.company.presentation.exception.CompanyNotFoundException;
 import team.mjk.agent.domain.company.presentation.exception.InvalidInvitationCode;
+import team.mjk.agent.domain.email.infrastructure.EmailSender;
 import team.mjk.agent.domain.invitation.InvitationCodeProvider;
 import team.mjk.agent.domain.invitation.domain.Invitation;
 import team.mjk.agent.domain.invitation.domain.InvitationRepository;
 import team.mjk.agent.domain.member.domain.Member;
 import team.mjk.agent.domain.member.domain.MemberRepository;
 import team.mjk.agent.domain.member.presentation.exception.MemberNotFoundException;
+import team.mjk.agent.global.util.EmailMessageBuilder;
 
 @RequiredArgsConstructor
 @Service
@@ -25,18 +30,33 @@ public class CompanyService {
   private final InvitationRepository invitationRepository;
   private final CompanyRepository companyRepository;
   private final MemberRepository memberRepository;
+  private final EmailSender emailSender;
+  private final EmailMessageBuilder emailMessageBuilder;
 
-  public Invitation createInvitationCode(Long memberId) {
+  @Value("${spring.mail.username}")
+  private String senderEmail;
+
+  public CompanyInvitationEmailResponse createInvitationCodeAndSendEmail(Long memberId, CompanyInvitationEmailRequest request) {
     Member member = memberRepository.findByMemberId(memberId)
         .orElseThrow(MemberNotFoundException::new);
 
     Long companyId = member.getCompany().getId();
-    // TODO: 커스텀 예외 처리 하세용
+
     if (companyRepository.findById(companyId).isEmpty()) {
       throw new CompanyNotFoundException();
     }
 
-    return invitationCodeProvider.create(companyId);
+    Invitation invitation = invitationCodeProvider.create(companyId);
+
+    String subject = emailMessageBuilder.buildInvitationSubject();
+    String content = emailMessageBuilder.buildInvitationMessage(invitation.getCode());
+
+    emailSender.send(senderEmail, request.email(), subject, content);
+
+    return CompanyInvitationEmailResponse.builder()
+            .email(request.email())
+            .invitationCode(invitation.getCode())
+            .build();
   }
 
   @Transactional
