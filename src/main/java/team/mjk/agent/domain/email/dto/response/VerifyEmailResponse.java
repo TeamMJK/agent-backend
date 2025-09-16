1fdcc9642569c757a7dd3726854e9c8570ffa3f0
y22jun <tlsdpwns0612@naver.com>
2025-09-16 16:08:21 +0900

    docs: 이메일 인증 관련 dto 스키마 추가

--- src/main/java/team/mjk/agent/domain/email/dto/response/VerifyEmailResponse.java
+++ src/main/java/team/mjk/agent/domain/email/dto/response/VerifyEmailResponse.java
@@ -1,11 +1,13 @@
 package team.mjk.agent.domain.email.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "이메일 인증 검증 응답 DTO")
 public record VerifyEmailResponse(
 
+        @Schema(description = "인증 성공 여부", example = "true")
         boolean success
 
-) {
-}
+) {}
