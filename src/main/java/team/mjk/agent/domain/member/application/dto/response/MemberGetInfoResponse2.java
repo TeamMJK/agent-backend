4760941ae98c1db9c0954a22b8866ea3f9419de4
y22jun <tlsdpwns0612@naver.com>
2025-11-18 01:54:23 +0900

    fix: enum-string 변환 문제 해결

--- src/main/java/team/mjk/agent/domain/member/application/dto/response/MemberGetInfoResponse2.java
+++ src/main/java/team/mjk/agent/domain/member/application/dto/response/MemberGetInfoResponse2.java
@@ -6,26 +6,26 @@ import team.mjk.agent.domain.member.domain.Member;
 import team.mjk.agent.global.util.KmsUtil;
 
 @Builder
-public record MemberInfoGetResponse(
-    String name,
-    String email,
-    String firstName,
-    String lastName,
-    String phoneNumber,
-    String gender,
-    String birthDate,
-    String passportNumber,
-    String passportExpireDate
+public record MemberGetInfoResponse2(
+        String name,
+        String email,
+        String firstName,
+        String lastName,
+        String phoneNumber,
+        Gender gender,
+        String birthDate,
+        String passportNumber,
+        String passportExpireDate
 ) {
 
-    public static MemberInfoGetResponse from(Member member, KmsUtil kmsUtil) {
-        return MemberInfoGetResponse.builder()
+    public static MemberGetInfoResponse2 from(Member member, KmsUtil kmsUtil) {
+        return MemberGetInfoResponse2.builder()
                 .name(member.getName())
                 .firstName(kmsUtil.decrypt(member.getFirstName()))
                 .lastName(kmsUtil.decrypt(member.getLastName()))
                 .email(member.getEmail())
                 .phoneNumber(kmsUtil.decrypt(member.getPhoneNumber()))
-                .gender(member.getGender().name())
+                .gender(member.getGender())
                 .birthDate(kmsUtil.decrypt(member.getBirthDate()))
                 .passportNumber(kmsUtil.decrypt(member.getPassport().getPassportNumber()))
                 .passportExpireDate(kmsUtil.decrypt(member.getPassport().getPassportExpireDate()))
--- src/main/java/team/mjk/agent/domain/member/infrastructure/MemberJpaRepository.java
+++ src/main/java/team/mjk/agent/domain/member/infrastructure/MemberJpaRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import team.mjk.agent.domain.company.domain.Company;
+import team.mjk.agent.domain.member.application.dto.response.MemberGetInfoResponse2;
 import team.mjk.agent.domain.member.application.dto.response.MemberInfoGetResponse;
 import team.mjk.agent.domain.member.domain.Member;
 
@@ -22,14 +23,14 @@ public interface MemberJpaRepository extends JpaRepository<Member, Long> {
 
     List<Member> findAllByCompanyId(@Param("companyId") Long companyId);
 
-    @Query("select new team.mjk.agent.domain.member.application.dto.response.MemberInfoGetResponse(" +
+    @Query("select new team.mjk.agent.domain.member.application.dto.response.MemberGetInfoResponse2(" +
             "m.name, m.email, m.firstName, m.lastName, m.phoneNumber, m.gender, m.birthDate, " +
             "p.passportNumber, p.passportExpireDate) " +
             "from Member m " +
             "join m.company c " +
             "left join m.passport p " +
             "where c.id = :companyId")
-    List<MemberInfoGetResponse> findAllMemberInfoByCompanyId(@Param("companyId") Long companyId);
+    List<MemberGetInfoResponse2> findAllMemberInfoByCompanyId(@Param("companyId") Long companyId);
 
     long countByCompanyId(Long companyId);
 
