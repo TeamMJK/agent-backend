dd08628cd3be38e75c5eb784c74a7d4d17b236bf
y22jun <tlsdpwns0612@naver.com>
2025-11-18 02:13:59 +0900

    fix: enum-string 변환 문제 해결

--- src/main/java/team/mjk/agent/domain/company/dto/response/CompanyMemberListResponse.java
+++ src/main/java/team/mjk/agent/domain/company/dto/response/CompanyMemberListResponse.java
@@ -2,15 +2,11 @@ package team.mjk.agent.domain.company.dto.response;
 
 import java.util.List;
 import lombok.Builder;
-import team.mjk.agent.domain.member.application.dto.response.MemberGetInfoResponse2;
 import team.mjk.agent.domain.member.application.dto.response.MemberInfoGetResponse;
 
 @Builder
 public record CompanyMemberListResponse(
-    List<MemberInfoGetResponse> members,
-
-    List<MemberGetInfoResponse2> members2
-
+    List<MemberInfoGetResponse> members
 ) {
 
 }
