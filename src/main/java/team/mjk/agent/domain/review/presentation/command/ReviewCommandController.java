0e11c4cdb47642ab67117b36fe0d09693dcd68fa
y22jun <tlsdpwns0612@naver.com>
2025-12-05 18:20:12 +0900

    refactor: ReviewCommandController 구현

---
+++ src/main/java/team/mjk/agent/domain/review/presentation/command/ReviewCommandController.java
@@ -0,0 +1,31 @@
+package team.mjk.agent.domain.review.presentation.command;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.mjk.agent.domain.review.application.command.ReviewCommandService;
+import team.mjk.agent.domain.review.application.dto.response.ReviewSaveResponse;
+import team.mjk.agent.domain.review.presentation.request.ReviewSaveRequest;
+import team.mjk.agent.global.annotation.MemberId;
+
+@RequiredArgsConstructor
+@RequestMapping("/reviews")
+@RestController
+public class ReviewCommandController {
+
+    private final ReviewCommandService reviewCommandService;
+
+    @PostMapping
+    public ResponseEntity<ReviewSaveResponse> save(
+            @MemberId Long memberId,
+            @Valid @RequestBody ReviewSaveRequest request
+    ) {
+        ReviewSaveResponse response = reviewCommandService.saveReview(request.toReviewSaveServiceRequest(memberId));
+        return ResponseEntity.status(201).body(response);
+    }
+
+}
