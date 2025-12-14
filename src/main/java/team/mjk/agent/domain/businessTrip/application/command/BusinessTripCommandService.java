4fecb171307793560ac49915ec4208f296e60b9c
y22jun <tlsdpwns0612@naver.com>
2025-12-14 22:24:57 +0900

    refactor: BusinessTripCommandService 생성

---
+++ src/main/java/team/mjk/agent/domain/businessTrip/application/command/BusinessTripCommandService.java
@@ -0,0 +1,9 @@
+package team.mjk.agent.domain.businessTrip.application.command;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BusinessTripCommandService {
+}
