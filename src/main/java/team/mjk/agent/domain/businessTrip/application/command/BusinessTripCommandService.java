7d9cc70801056d4a856f10743292a2b6c7c6355d
y22jun <tlsdpwns0612@naver.com>
2025-12-15 22:25:00 +0900

    refactor: BusinessTripService save 로직 commandService로 이동

--- src/main/java/team/mjk/agent/domain/businessTrip/application/command/BusinessTripCommandService.java
+++ src/main/java/team/mjk/agent/domain/businessTrip/application/command/BusinessTripCommandService.java
@@ -2,8 +2,41 @@ package team.mjk.agent.domain.businessTrip.application.command;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.mjk.agent.domain.businessTrip.application.dto.request.BusinessTripSaveServiceRequest;
+import team.mjk.agent.domain.businessTrip.application.dto.response.BusinessTripSaveResponse;
+import team.mjk.agent.domain.businessTrip.domain.BusinessTrip;
+import team.mjk.agent.domain.businessTrip.domain.BusinessTripRepository;
+import team.mjk.agent.domain.company.domain.Company;
+import team.mjk.agent.domain.member.domain.Member;
+import team.mjk.agent.domain.member.domain.MemberRepository;
 
 @RequiredArgsConstructor
 @Service
 public class BusinessTripCommandService {
+
+    private final BusinessTripRepository businessTripRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public BusinessTripSaveResponse save(BusinessTripSaveServiceRequest request) {
+        Member member = memberRepository.findByMemberId(request.memberId());
+        Company company = member.getValidatedCompany();
+
+        BusinessTrip businessTrip = BusinessTrip.create(
+                request.departDate(),
+                request.arriveDate(),
+                request.destination(),
+                request.names(),
+                member.getName(),
+                company.getId(),
+                request.serviceType()
+        );
+        businessTripRepository.save(businessTrip);
+
+        return BusinessTripSaveResponse.builder()
+                .businessTripId(businessTrip.getId())
+                .build();
+    }
+
 }
