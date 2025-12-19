d1f28ad805ffa8f37a785169725c151daf1eac2f
y22jun <tlsdpwns0612@naver.com>
2025-12-19 22:38:20 +0900

    refactor: saveMcp 메서드 BusinessTripCommandService로 이동

--- src/main/java/team/mjk/agent/domain/businessTrip/application/command/BusinessTripCommandService.java
+++ src/main/java/team/mjk/agent/domain/businessTrip/application/command/BusinessTripCommandService.java
@@ -8,8 +8,14 @@ import team.mjk.agent.domain.businessTrip.application.dto.response.BusinessTripS
 import team.mjk.agent.domain.businessTrip.domain.BusinessTrip;
 import team.mjk.agent.domain.businessTrip.domain.BusinessTripRepository;
 import team.mjk.agent.domain.company.domain.Company;
+import team.mjk.agent.domain.companyworkspace.application.query.CompanyWorkspaceQueryService;
+import team.mjk.agent.domain.companyworkspace.domain.Workspace;
+import team.mjk.agent.domain.mcp.McpService;
+import team.mjk.agent.domain.mcp.McpServiceRegistry;
 import team.mjk.agent.domain.member.domain.Member;
 import team.mjk.agent.domain.member.domain.MemberRepository;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -17,6 +23,8 @@ public class BusinessTripCommandService {
 
     private final BusinessTripRepository businessTripRepository;
     private final MemberRepository memberRepository;
+    private final CompanyWorkspaceQueryService companyWorkspaceQueryService;
+    private final McpServiceRegistry mcpServiceRegistry;
 
     @Transactional
     public BusinessTripSaveResponse save(BusinessTripSaveServiceRequest request) {
@@ -37,6 +45,23 @@ public class BusinessTripCommandService {
         return BusinessTripSaveResponse.builder()
                 .businessTripId(businessTrip.getId())
                 .build();
+    }
+
+    //Controller(저장) -> saveMcp 호출 -> save 호출
+    @Transactional
+    public List<Workspace> saveMcp(BusinessTripSaveServiceRequest request) {
+        Member member = memberRepository.findByMemberId(request.memberId());
+        Company company = member.getValidatedCompany();
+        List<Workspace> workspaces = companyWorkspaceQueryService.getWorkspacesByCompanyId(company.getId());
+
+        for (Workspace workspace : workspaces) {
+            List<McpService> mcpServices = mcpServiceRegistry.getServices(workspace);
+            for (McpService mcpService : mcpServices) {
+                mcpService.createBusinessTrip(request, company.getId(), member.getName());
+            }
+        }
+
+        return workspaces;
     }
 
 }
