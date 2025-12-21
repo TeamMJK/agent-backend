af5f049aef88a66685ac680f8c2354c3d9f439d6
y22jun <tlsdpwns0612@naver.com>
2025-12-21 21:37:51 +0900

    refactor: getAllBusinessTrip 메서드 BusinessTripQueryService로 이동

--- src/main/java/team/mjk/agent/domain/businessTrip/application/query/BusinessTripQueryService.java
+++ src/main/java/team/mjk/agent/domain/businessTrip/application/query/BusinessTripQueryService.java
@@ -3,12 +3,15 @@ package team.mjk.agent.domain.businessTrip.application.query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.mjk.agent.domain.businessTrip.application.dto.response.BusinessTripGetAllResponse;
 import team.mjk.agent.domain.businessTrip.application.dto.response.BusinessTripGetResponse;
 import team.mjk.agent.domain.businessTrip.domain.BusinessTrip;
 import team.mjk.agent.domain.businessTrip.domain.BusinessTripRepository;
 import team.mjk.agent.domain.company.domain.Company;
 import team.mjk.agent.domain.member.domain.Member;
 import team.mjk.agent.domain.member.domain.MemberRepository;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -34,6 +37,20 @@ public class BusinessTripQueryService {
                 .names(businessTrip.getNames())
                 .writer(businessTrip.getWriter())
                 .serviceType(businessTrip.getServiceType())
+                .build();
+    }
+
+    @Transactional(readOnly = true)
+    public BusinessTripGetAllResponse getAllBusinessTrip(Long memberId) {
+        Member member = memberRepository.findByMemberId(memberId);
+        Company company = member.getValidatedCompany();
+
+        List<BusinessTrip> businessTripList = businessTripRepository.findAllByCompanyId(
+                company.getId()
+        );
+
+        return BusinessTripGetAllResponse.builder()
+                .businessTripList(businessTripList)
                 .build();
     }
 
