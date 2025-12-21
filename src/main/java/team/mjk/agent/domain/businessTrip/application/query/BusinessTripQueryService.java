544559cbed7141f27bd6e5778bd0fc29b4685cee
y22jun <tlsdpwns0612@naver.com>
2025-12-21 21:35:36 +0900

    feat: BusinessTripQueryService 생성

---
+++ src/main/java/team/mjk/agent/domain/businessTrip/application/query/BusinessTripQueryService.java
@@ -0,0 +1,9 @@
+package team.mjk.agent.domain.businessTrip.application.query;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BusinessTripQueryService {
+}
